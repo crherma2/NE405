138669cea4186c4f0dedfd71479a1bac3e5e4b23
Bryan Herman <bherman@mit.edu>
2014-02-02 17:39:46 -0500

    added axis labels

--- postprocess.py
+++ postprocess.py
@@ -15,6 +15,9 @@ set output '{outfile}.pdf'
 
 unset key
 
+set xlabel '{xlabel}'
+set ylabel '{ylabel}'
+
 plot '-' using 1:2 with lines lw 4.0
 {data}
 """
@@ -29,15 +32,18 @@ class Data(object):
         self.pts.append(pt)
 
 class Plot(object):
-    def __init__(self, x, y):
+    def __init__(self, x, xlabel, y, ylabel):
         self.x=x
+        self.xlabel = xlabel
         self.y=y
+        self.ylabel = ylabel
     def write_gnuplot(self, outfile):
 
        data_str = ''
        for i in range(len(self.x)):
            data_str += '{x} {y}\n'.format(x = self.x[i], y = self.y[i])
-       file_str = plot_template.format(outfile=outfile, data=data_str)
+       file_str = plot_template.format(outfile=outfile, data=data_str,
+                                       xlabel=self.xlabel, ylabel=self.ylabel)
 
        with open(outfile+'.plot','w') as fh:
            fh.write(file_str)
@@ -61,6 +67,7 @@ def main(filename):
     for key in name_dict.keys():
         keyns = key.replace(' ','')
         lines[0] = lines[0].replace(key, keyns)
+        name_dict.update({keyns:name_dict.pop(key)}) # change key in name dictionary to no spaces
 
     # looping through lines
     i = 0
@@ -71,19 +78,17 @@ def main(filename):
             sline=lines[i].split()
             idx = 0
             for key in sline:              
-                data_dict.update({key:Data(key,idx)})
+                data_dict.update({key:Data(name_dict[key],idx)})
                 idx +=1
             i +=1
             continue
-
 
         # for all other lines append data points
         sline=lines[i].split()
         for key in data_dict.keys():
             data_dict[key].add_datapt(sline[data_dict[key].idx])
             
-
-        i +=1
+        i +=1 # next line
     
     # create plot objects
     for key in data_dict.keys():
@@ -91,8 +96,10 @@ def main(filename):
             continue
         
         x = data_dict['time'].pts
+        xlabel = data_dict['time'].name
         y = data_dict[key].pts
-        plot_dict.update({key:Plot(x,y)})
+        ylabel = data_dict[key].name
+        plot_dict.update({key:Plot(x, xlabel, y, ylabel)})
     
     # run plots
     for key in plot_dict.keys():
