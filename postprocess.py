fbe86830573473ad855388735a34587e88046a7f
Christopher Herman <crherma2@ncsu.edu>
2014-02-02 15:38:45 -0500

    parse data

--- postprocess.py
+++ postprocess.py
@@ -8,8 +8,9 @@ plot_dict=OrderedDict()
 
 
 class Data(object):
-    def __init__(self, name):
+    def __init__(self, name, idx):
         self.name=name
+        self.idx=idx
         self.pts=[]
 
     def add_datapt(self, pt):
@@ -22,7 +23,32 @@ def main(filename):
     with open(filename,'r') as fh:
         lines=fh.read().splitlines()
 
-    print lines
+    # looping through lines
+    i = 0
+    while i<len(lines):
+
+        # first line create data objects
+        if i==0:
+            sline=lines[i].split()
+            idx = 0
+            for key in sline:              
+                data_dict.update({key:Data(key,idx)})
+            i +=1
+            continue
+
+
+        # for all other lines append data points
+        sline=lines[i].split()
+        for key in data_dict.keys():
+            data_dict[key].add_datapt(sline[data_dict[key].idx])
+
+
+        i +=1
+
+    print data_dict['time'].name
+    print data_dict['time'].idx
+    print data_dict['time'].pts
+
 
 if __name__ == '__main__':
     filename=sys.argv[1]
